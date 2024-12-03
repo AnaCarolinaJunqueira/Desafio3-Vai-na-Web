83cae267e372e171f8429f4ad05fbc8952417410
Ana Carolina Junqueira e Silva <acjunqueiras@gmail.com>
2024-12-02 22:55:23 -0300

    criação da pag Quero Doar

--- src/Pages/QueroDoar/QueroDoar.jsx
+++ src/Pages/QueroDoar/QueroDoar.jsx
@@ -1,9 +1,22 @@
-
+import S from './queroDoar.module.scss'
+import livroVector from '../../assets/livroVector.png'
 
 export default function QueroDoar(){
     return(
         <section>
-            <h2>Quero Doar</h2>
+            <section>
+                <h2>Por favor, preencha o formulário com suas informações eas informações do Livro</h2>
+                <form action="">
+                    <div>
+                        <img src={livroVector} alt="Imagem mais simplificada de um livro aberto" />
+                        <h3>Informações do Livro</h3>
+                    </div>
+                    <input type="text" placeholder='Titulo' />
+                    <input type="text" placeholder='Categoria' />
+                    <input type="text" placeholder='Autor' />
+                    <input type="text" placeholder='Link da Imagem'/>
+                </form>
+            </section>
         </section>
     )
 }
