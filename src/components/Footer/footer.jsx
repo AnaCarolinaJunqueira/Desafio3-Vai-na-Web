3515a47d44312f256a9426987e4d5c582962365e
Ana Carolina Junqueira e Silva <acjunqueiras@gmail.com>
2024-12-02 21:35:11 -0300

    criação do footer.scss

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -3,20 +3,22 @@ import twitter from '../../assets/twitter.png'
 import youtube from '../../assets/youtube.png'
 import linkedin from '../../assets/youtube.png'
 import instagram from '../../assets/instagram.png'
+import S from './footer.module.scss'
+
 export default function footer(){
     return(
         <footer>
-            <section>
+            <section className={S.boxSocial}>
                 <h3>4002-8922</h3>
                 <nav>
-                    <a href={facebook}></a>
-                    <a href={twitter}></a>
-                    <a href={youtube}></a>
-                    <a href={linkedin}></a>
-                    <a href={instagram}></a>
+                    <a href=""><img src={facebook} alt="logo do facebook" /></a>
+                    <a href=""><img src={twitter} alt="logo do twitter" /></a>
+                    <a href=""><img src={youtube} alt="logo do youtube" /></a>
+                    <a href=""><img src={linkedin} alt="logo do linkedin" /></a>
+                    <a href=""><img src={instagram} alt="logo do instagram" /></a>
                 </nav>
             </section>
-            <section>
+            <section className={S.boxEnd}>
                 <p>Layout desenvolvido pela Vai Na Web para fins educativos - 2024</p>
             </section>
         </footer>
