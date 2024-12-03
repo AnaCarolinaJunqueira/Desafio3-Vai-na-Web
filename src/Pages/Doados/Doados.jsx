097cb7d1cc544ef076d43dce5fcac2a9624da475
Ana Carolina Junqueira e Silva <acjunqueiras@gmail.com>
2024-12-02 23:28:08 -0300

    criação da pag doados

--- src/Pages/Doados/Doados.jsx
+++ src/Pages/Doados/Doados.jsx
@@ -1,9 +1,18 @@
-
+import S from './doados.module.scss'
+import livro from '../../assets/livro.png'
 
 export default function Doados(){
     return(
         <section>
-            <h2>Doados</h2>
+            <h2>Livros Doados</h2>
+            <section>
+                <article>
+                    <img src={livro} alt="imagem do livro o protagonista" />
+                    <h3>O protagonista</h3>
+                    <p>Susanne Andrade</p>
+                    <p>Ficção</p>
+                </article>
+            </section>
         </section>
     )
 }
