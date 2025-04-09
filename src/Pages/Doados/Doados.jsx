7ad9873f77bba7ad37076dd2c8b1fc83d102f87b
Ana Carolina Junqueira e Silva <acjunqueiras@gmail.com>
2025-04-09 02:04:31 -0300

    alterações finais

--- src/Pages/Doados/Doados.jsx
+++ src/Pages/Doados/Doados.jsx
@@ -1,17 +1,42 @@
 import S from './doados.module.scss'
 import livro from '../../assets/livro.png'
+import axios from 'axios'
+import { useState, useEffect} from 'react'
 
 export default function Doados(){
-    return(
+    //estado para armazenar os livros recebidos da API
+    const [livros, setLivros] = useState([])
+
+    //função assíncrona para buscar os livros na API
+    const getLivros = async () => {
+        try{
+            const response = await axios.get("https://teste-deploy-bkai.onrender.com/livros")
+            setLivros(response.data)
+        } catch (error) {
+            console.error("Erro ao buscar livros: ", error)
+        }
+    }
+
+    //useeffect para chamar a função de busca ao montar o componente
+    useEffect(() => {
+        getLivros()
+    }, [])
+
+    return (
         <section className={S.boxDoados}>
             <h2>Livros Doados</h2>
             <section className={S.boxBooks}>
-                <article>
-                    <img src={livro} alt="imagem do livro o protagonista" />
-                    <h3>O protagonista</h3>
-                    <p>Susanne Andrade</p>
-                    <p>Ficção</p>
-                </article>
+                {/* Livro fixo */}
+
+                {/* Renderização dos livros recebidos da API */}
+                {livros.map((item)=> (
+                    <article key={item.id}>
+                        <img src={item.image_url} alt={item.titulo} />
+                        <h3>{item.titulo}</h3>
+                        <p>{item.autor}</p>
+                        <p>{item.categoria}</p>
+                    </article>
+                ))}
             </section>
         </section>
     )
